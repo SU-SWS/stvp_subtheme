d4643734d9002e250684692c832ba692ff5f5a7a
pookmish <pookmish@stanford.edu>
2026-03-18 04:24:25 +0700

    STVP26-133: Create algolia media search components (#30)

---
+++ pdb_components/media-search/src/hooks/useAccordion.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import {HTMLAttributes} from "preact"
+import {useBoolean} from "usehooks-ts"
+import {useId} from "preact/compat";
+
+type Props = {
+  initiallyVisible?: boolean
+  panelId?: string
+  buttonId?: string
+}
+
+const useAccordion = ({initiallyVisible, panelId, buttonId}: Props = {}) => {
+  const {
+    value: expanded,
+    setTrue: expandAccordion,
+    setFalse: collapseAccordion,
+    toggle: toggleExpanded,
+  } = useBoolean(initiallyVisible)
+  const id = useId()
+
+  const buttonProps: HTMLAttributes<HTMLButtonElement> = {
+    "aria-controls": panelId || `${id}--panel`,
+    "aria-expanded": expanded,
+    id: buttonId || `${id}--button`,
+    onClick: toggleExpanded,
+  }
+  const panelProps: HTMLAttributes<HTMLDivElement> = {
+    "aria-labelledby": buttonId || `${id}--button`,
+    id: panelId || `${id}--panel`,
+    role: "region",
+    className: expanded ? "block" : "hidden",
+  }
+  return {buttonProps, panelProps, expanded, toggleExpanded, expandAccordion, collapseAccordion}
+}
+export default useAccordion
